9db081a50d30786b842e5ba4ba0da63b307c80df
YangHongYu <319139806@qq.com>
2023-06-07 16:55:47 +0800

    Update MsmServiceImpl.java

--- service/service_msm/src/main/java/com/example/msmservice/service/impl/MsmServiceImpl.java
+++ service/service_msm/src/main/java/com/example/msmservice/service/impl/MsmServiceImpl.java
@@ -23,14 +23,14 @@ public class MsmServiceImpl implements MsmService {
     public boolean send(Map<String, Object> param, String phone) {
         if (StringUtils.isEmpty(phone)) return false;
         DefaultProfile profile =
-                DefaultProfile.getProfile("cn-shanghai", "LTAI5t8wX9VSQCT3NdjFyWEq",
-                        "2I9vO7LkZ44731hOnzQ0z8elW1Vr1A");
+                DefaultProfile.getProfile("cn-shanghai", "",
+                        "");
         IAcsClient client = new DefaultAcsClient(profile);
 
         SendSmsRequest request = new SendSmsRequest();
         request.setPhoneNumbers(phone);//接收短信的手机号码
         request.setSignName("阿里云短信测试");//短信签名名称
-        request.setTemplateCode("SMS_154950909");//短信模板CODE
+        request.setTemplateCode("");//短信模板CODE
         request.setTemplateParam(JSONObject.toJSONString(param));//短信模板变量对应的实际值
 
         try {
